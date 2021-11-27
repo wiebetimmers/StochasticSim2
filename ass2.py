326d5607807df0f1d41a0d7c59e2c36f56a7799c
wjr-timmers <67547499+wjr-timmers@users.noreply.github.com>
2021-11-27 15:06:45 +0100

    test

---
+++ ass2.py
@@ -0,0 +1,86 @@
+"""
+Bank renege example
+
+Covers:
+
+- Resources: Resource
+- Condition events
+
+Scenario:
+  A counter with a random service time and customers who renege. Based on the
+  program bank08.py from TheBank tutorial of SimPy 2. (KGM)
+
+"""
+import random
+from tqdm import tqdm
+import statistics as st
+import simpy
+import seaborn as sns
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(6,4), dpi=300)
+
+
+RANDOM_SEED = 42
+NEW_CUSTOMERS = 10000  # Total number of customers
+INTERVAL_CUSTOMERS = 12.0  # Generate new customers roughly every x seconds
+INTERVAL_SERVICE = 10.0 # Service takes roughly 10 seconds of time
+
+ARR_RATE = 1/INTERVAL_CUSTOMERS
+SERV_RATE = 1/ INTERVAL_SERVICE
+CAP = 1
+
+
+def source(env, number, counter):
+    soj_times = []
+    """Source generates customers randomly"""
+    for i in tqdm(range(number)):
+        c = customer(env, 'Customer%02d' % i, counter)
+        soj_time = yield env.process(c)
+        soj_times.append(soj_time)
+        t = random.expovariate(ARR_RATE)
+        yield env.timeout(t)
+
+    print('Mean soj time: ', st.mean(soj_times))
+    sns.displot(soj_times)
+    plt.xlabel('sojourn time')
+    plt.savefig('sojourn_displot_%s.jpg'%CAP)
+
+def customer(env, name, counter):
+    """Customer arrives, is served and leaves."""
+    arrive = env.now
+    #print('%7.4f %s: Here I am' % (arrive, name))
+
+    with counter.request() as req:
+        results = yield req | env.timeout(1000)
+
+        wait = env.now - arrive
+
+        if req in results:
+            # We got to the counter
+            #print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+            # We get served
+            tib = random.expovariate(SERV_RATE)
+            yield env.timeout(tib)
+            end = env.now
+            soj_time = end-arrive
+            return soj_time
+            #print('%7.4f %s: Sojourn Time' % (end-arrive, name))
+            #print('%7.4f %s: Finished' % (env.now, name))
+
+
+# Setup and start the simulation
+random.seed(RANDOM_SEED)
+env = simpy.Environment()
+
+# Start processes and run
+capacities = [1,2,4]
+for cap in capacities:
+    CAP = cap
+    print('Bank renege')
+    system_load = ARR_RATE / (CAP * SERV_RATE)
+    print('System load: ', system_load)
+    print('E(L) = %f' % ((system_load)/(1-system_load)))
+    print('E(S) = %f' % ((1/SERV_RATE)/(1-system_load)))
+    counter = simpy.Resource(env, capacity=CAP)
+    env.process(source(env, NEW_CUSTOMERS, counter))
+    env.run()
